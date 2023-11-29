456a4a7ab5053a432133ddfc162607e948f74508
Jaroslaw Janowski <jarek@MacBook-Pro-Jaroslaw.local>
2023-11-29 22:23:37 +0100

    add auth

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex-center primary-gradient">
-      <h1 className="h1-bold">Next.js 14 I&apos;am coming!</h1>
-    </main>
-  );
-}
