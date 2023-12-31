b0b44fa87ba5e02f141cf1c0f3995b0fa9fddf32
Jaroslaw Janowski <jarjanowski95@gmail.com>
2023-12-31 15:17:23 +0100

    feat: Community & Tags pages

---
+++ app/(root)/tags/page.tsx
@@ -0,0 +1,76 @@
+import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+import LocalSearch from "@/components/shared/search/LocalSearch";
+import { TagFilters } from "@/constants/filters";
+import { getAllTags } from "@/lib/actions/tag.actions";
+import React from "react";
+import Link from "next/link";
+
+const Page = async () => {
+  const result = await getAllTags({});
+
+  console.log(result.tags);
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Tags</h1>
+
+      <div
+        className="mt-11 flex flex-wrap justify-between
+      gap-5 max-sm:flex-col sm:items-center"
+      >
+        <LocalSearch
+          route="/tags"
+          iconPosition="left"
+          imageSrc="/assets/icons/search.svg"
+          placeholder="Search by tag name"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={TagFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          placeholder="Select a Filter"
+        />
+      </div>
+      <section className="mt-12 flex flex-wrap gap-4">
+        {result.tags.length > 0 ? (
+          result.tags.map((tag) => (
+            <Link
+              key={tag._id}
+              href={`/tags/${tag._id}`}
+              className="shadow-light100_darknone"
+            >
+              <article className="background-light900_dark200 light-border flex w-full flex-col justify-center rounded-2xl border px-8 py-10 sm:w-[260px]">
+                <div className="background-light800_dark400 w-fit rounded px-5 pt-1.5">
+                  <p className="paragraph-semibold text-dark300_light900">
+                    {tag.name}
+                  </p>
+                </div>
+                <div className="mt-[18px]">
+                  <p className="text-dark500_light700 small-regular">
+                    {tag.description}
+                  </p>
+                </div>
+
+                <p className="small-medium text-dark400_light500 mt-3.5">
+                  <span className="body-semibold primary-text-gradient mr-2.5">
+                    {tag.question.length}+
+                  </span>
+                  Questions
+                </p>
+              </article>
+            </Link>
+          ))
+        ) : (
+          <NoResult
+            title="No tags Found"
+            description="It looks like there are no tags found."
+            link="/ask-question"
+            linkTitle="Ask a question"
+          />
+        )}
+      </section>
+    </>
+  );
+};
+
+export default Page;
